01a54a8e5ed2a2276e26becfe4ed75b1d2f65ce6
EvanQP <69576334+EvanQP@users.noreply.github.com>
2021-02-16 16:16:08 +0800

    Trying to fix LINK error - evan
    hotfix to comment sound.cpp and .h

--- Application/Sound.h
+++ Application/Sound.h
@@ -1,15 +1,15 @@
 #pragma once
-#include "Dependencies/irrKlang-64bit-1.6.0/include/irrKlang.h"
-#include <iostream>
-#include <conio.h>
-
-using namespace irrklang;
-
-#pragma comment(lib, "irrKlang.lib")
-
-class Sound
-{
-public:
-	ISoundEngine* engine;
-	Sound();
-};
+//#include "Dependencies/irrKlang-1.6.0/include/irrKlang.h"
+//#include <iostream>
+//#include <conio.h>
+//
+//using namespace irrklang;
+//
+//#pragma comment(lib, "irrKlang.lib")
+//
+//class Sound
+//{
+//public:
+//	ISoundEngine* engine;
+//	Sound();
+//};
