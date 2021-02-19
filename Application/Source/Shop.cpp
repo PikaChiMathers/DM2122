533baad369b8c681cea0f49ad3d37e23db651b5b
ZACHARY TAN <203202U@mymail.nyp.edu.sg>
2021-02-19 13:51:31 +0800

    shop class - zach

---
+++ Application/Source/Shop.cpp
@@ -0,0 +1,21 @@
+#include "Shop.h"
+
+Shop::Shop()
+{
+
+}
+
+Shop::~Shop()
+{
+
+}
+
+void Shop::upgrade(int upgradeType)
+{
+    upgradeLevel[upgradeType] += 1;
+}
+
+int Shop::getUpgradeLevel(int upgradeType)
+{
+    return upgradeLevel[upgradeType];
+}
