23028b02c4f75309ab295cc4d30b355dd693ef81
JLT3k <200628a@mymail.nyp.edu.sg>
2021-02-21 17:31:57 +0800

    Bus can now steer. - JL

--- Application/Source/Bus.cpp
+++ Application/Source/Bus.cpp
@@ -15,27 +15,35 @@ Bus::~Bus()
 void Bus::GameObjectUpdate(double dt)
 {
 	float xVelocity = 0, zVelocity = 0;
+	Vector3 velocity(xVelocity, 0, zVelocity);
 	GetPhysics()->SetDrag(1);
 	if (Application::IsKeyPressed('T'))
 	{
-		zVelocity -= acceleration * dt;
+		//zVelocity -= acceleration * dt;
+		//GetPhysics()->AddVelocity(GetFoward() * acceleration * dt);
+		velocity += GetFoward() * acceleration * dt;
 	}
 	if (Application::IsKeyPressed('G'))
 	{
-		zVelocity += acceleration * dt;
+		//zVelocity += acceleration * dt;
+		//GetPhysics()->AddVelocity(GetFoward() * -acceleration * dt);
+		velocity -= GetFoward() * acceleration * dt;
 	}
 	if (Application::IsKeyPressed('F'))
 	{
-		xVelocity -= acceleration * dt;
+		//xVelocity -= acceleration * dt;
+		//GetPhysics()->AddVelocity(GetRight() * -acceleration * dt);
+		if (velocity.Length() != 0) SetRotateY(GetRotateY() + 90 * dt);
 	}
 	if (Application::IsKeyPressed('H'))
 	{
-		xVelocity += acceleration * dt;
+		//xVelocity += acceleration * dt;
+		//GetPhysics()->AddVelocity(GetRight() * acceleration * dt);
+		if (velocity.Length() != 0) SetRotateY(GetRotateY() - 90 * dt);
 	}
-	Vector3 velocity(xVelocity, 0, zVelocity);
-	if (velocity.Length() > acceleration) velocity* (50 / velocity.Length());
-	GetPhysics()->AddVelocity(Vector3(xVelocity, 0, zVelocity));
-	if (xVelocity == 0 && zVelocity == 0) GetPhysics()->SetDrag(brakePower);
+	if (velocity.Length() > acceleration) velocity *= (50 / velocity.Length());
+	GetPhysics()->AddVelocity(velocity);
+	if (velocity.Length() == 0) GetPhysics()->SetDrag(brakePower);
 }
 
 std::string Bus::Type()
