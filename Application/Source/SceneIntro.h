09cca5a9a852435814ac9659968de536bf0dcf22
EvanQP <69576334+EvanQP@users.noreply.github.com>
2021-02-22 10:35:49 +0800

    made scene intro class and added .h code -Evan

---
+++ Application/Source/SceneIntro.h
@@ -0,0 +1,166 @@
+#ifndef SCENE_TEST
+#define SCENE_TEST
+
+#include "Scene.h"
+#include "Camera4.h"
+#include "Mesh.h"
+#include "MatrixStack.h"
+#include "Vertex.h"
+#include "Light.h"
+
+#include "GameObject.h"
+#include "Maps.h"
+
+
+#include "Dialogue.h"
+
+#include <sstream>
+
+class SceneTester : public Scene
+{
+public:
+	SceneTester();
+	~SceneTester();
+
+	virtual void Init();
+	virtual void Update(double dt);
+	virtual void Render();
+	virtual void Exit();
+
+	float rotateAngle;
+	float translateX, translateY;
+	float scaleAll;
+
+	float fps;
+
+	float x_value, y_value, z_value;
+
+	int translateXDir, translateYDir, rotateDir, scaleDir;
+
+	Color red, blue, green, pink, Lblue, purple, orange, yellow, cyan, magenta, moonshade;
+
+	bool scene_change;
+	bool coin_collect; // To stop rendering of coin after collected
+
+	int UI_height, UI_width;
+
+	int colCount = 0;
+	bool colEnter = false;
+
+
+	enum GEOMETRY_TYPE //added (Step 1)
+	{
+		GEO_AXES = 0,
+
+		GEO_QUAD,
+		GEO_CUBE,
+		GEO_GOOSE,
+
+		GEO_LIGHTBALL,
+		GEO_SPHERE,
+		GEO_CIRCLE,
+
+		GEO_LEFT,
+		GEO_RIGHT,
+		GEO_TOP,
+		GEO_BOTTOM,
+		GEO_FRONT,
+		GEO_BACK,
+
+		GEO_TEXT,
+
+		GEO_TEST,
+
+		NUM_GEOMETRY,
+	};
+
+	enum UNIFORM_TYPE
+	{
+		U_MVP = 0,
+		U_MODELVIEW,
+		U_MODELVIEW_INVERSE_TRANSPOSE,
+		U_MATERIAL_AMBIENT,
+		U_MATERIAL_DIFFUSE,
+		U_MATERIAL_SPECULAR,
+		U_MATERIAL_SHININESS,
+		U_LIGHT0_POSITION,
+		U_LIGHT0_COLOR,
+		U_LIGHT0_POWER,
+		U_LIGHT0_KC,
+		U_LIGHT0_KL,
+		U_LIGHT0_KQ,
+		U_LIGHT0_TYPE,
+		U_LIGHT0_SPOTDIRECTION,
+		U_LIGHT0_COSCUTOFF,
+		U_LIGHT0_COSINNER,
+		U_LIGHT0_EXPONENT,
+
+
+		U_LIGHT1_POSITION,
+		U_LIGHT1_COLOR,
+		U_LIGHT1_POWER,
+		U_LIGHT1_KC,
+		U_LIGHT1_KL,
+		U_LIGHT1_KQ,
+		U_LIGHT1_TYPE,
+		U_LIGHT1_SPOTDIRECTION,
+		U_LIGHT1_COSCUTOFF,
+		U_LIGHT1_COSINNER,
+		U_LIGHT1_EXPONENT,
+
+
+		U_LIGHTENABLED,
+		U_NUMLIGHTS,
+
+		U_COLOR_TEXTURE_ENABLED,
+		U_COLOR_TEXTURE,
+
+		U_TEXT_ENABLED,
+		U_TEXT_COLOR,
+
+		U_TOTAL,
+	};
+
+	enum LIGHT_TYPES
+	{
+		LIGHT1 = 0,
+		LIGHT2,
+		NUM_LIGHTS,
+	};
+
+
+private: //added (Step 2)
+	Camera4 camera;
+	Light lights[NUM_LIGHTS];
+	Maps map;
+
+	Dialogue* dialogue;
+
+	//GameObject gameObject;
+	GameObject goose;
+
+	unsigned m_vertexArrayID;
+	/*unsigned m_vertexBuffer[NUM_GEOMETRY];
+	unsigned m_colorBuffer[NUM_GEOMETRY];
+	unsigned m_indexBuffer[NUM_GEOMETRY];*/
+
+	Mesh* meshList[NUM_GEOMETRY];
+
+	MS modelStack, viewStack, projectionStack;
+
+
+	void RenderMesh(Mesh* mesh, bool enableLight);
+
+	void RenderSkybox();
+
+	void RenderText(Mesh* mesh, std::string text, Color color);
+
+	void RenderTextOnScreen(Mesh* mesh, std::string text, Color color, float size, float x, float y);
+
+	void RenderMeshOnScreen(Mesh* mesh, int x, int y, int sizex, int sizey);
+
+	unsigned m_parameters[U_TOTAL];
+	unsigned m_programID;
+};
+
+#endif
