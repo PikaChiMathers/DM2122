cf1a49dca9a4537d9dad9abd265c5edb9f3eb659
ZACHARY TAN <203202U@mymail.nyp.edu.sg>
2021-02-23 13:30:16 +0800

    progress on camera, jankiness to be fixed

--- Application/Source/SceneShop.h
+++ Application/Source/SceneShop.h
@@ -27,8 +27,6 @@ public:
 
 	float fps;
 
-	float x_value, y_value, z_value;
-
 	int translateXDir, translateYDir, rotateDir, scaleDir;
 
 	Color red, blue, green, pink, Lblue, purple, orange, yellow, cyan, magenta, moonshade;
