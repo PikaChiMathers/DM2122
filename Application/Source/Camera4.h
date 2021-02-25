4784521a0472e3b6cbc4d2048969a4a2f9faecfb
ZACHARY TAN <203202U@mymail.nyp.edu.sg>
2021-02-25 10:42:33 +0800

    done up cam 4 - zach

--- Application/Source/Camera4.h
+++ Application/Source/Camera4.h
@@ -24,9 +24,9 @@ public:
 	~Camera4();
 	virtual void Init(const Vector3& pos, const Vector3& target, const Vector3& up);
 	virtual void Update(double dt);
-	bool InBoundCheck(Vector3 pos);
 	virtual void Reset();
 
+	bool InBoundCheck(Vector3 pos);
 	void setBound(Vector3 botPosition, Vector3 topPosition);
 };
 
